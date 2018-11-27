90482d1d748abab422e245ae4e217ce40e66af58
NovelReader <jjn5938@louisiana.edu>
2018-11-27 08:43:47 -0600

    Add app to repo

---
+++ GamersOnlineApp/app/src/main/java/com/example/jjnjs/gamersonline/GamersOnline.java
@@ -0,0 +1,60 @@
+package com.example.jjnjs.gamersonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GamersOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamers_online);
+    }
+
+    public void getStuff(){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Toast.makeText(GamersOnline.this, "Made IT", Toast.LENGTH_LONG).show();
+                            //JSONObject jsonObject = new JSONObject(response)
+                            JSONObject json = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(GamersOnline.this);
+        requestQueue.add(stringRequest);
+    }
+}
