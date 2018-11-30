acb81a6817c8f6cdf3533d611cc9c0c34b9a94f0
Owsley Wood <oxw5863@louisiana.edu>
2018-11-29 18:07:58 -0600

    Finished login window
    Added more to the main activity

--- GamersOnlineApp/app/src/main/java/com/example/jjnjs/gamersonline/GamersOnline.java
+++ GamersOnlineApp/app/src/main/java/com/example/jjnjs/gamersonline/GamersOnline.java
@@ -1,8 +1,11 @@
 package com.example.jjnjs.gamersonline;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,32 +23,50 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Console;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GamersOnline extends AppCompatActivity {
 
-    private JSONObject json;
-    private boolean successLogin = false;
+    private boolean buttonPressed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamers_online);
 
+        //Code for debugging only
+        EditText emailIn = (EditText)findViewById(R.id.email);
+        TextView passIn = (TextView)findViewById(R.id.pass);
+        emailIn.setText("user1@user.com");
+        passIn.setText("user1");
+        //end Code for debugging
     }
 
     public void getStuff(){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "Http://18.219.55.178/GamersOnline/Methods/profile.php",
+        System.out.println("\n---\tCalled getStuff()\n");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://18.219.55.178/GamersOnline/Methods/profile.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            Toast.makeText(GamersOnline.this, "Made IT", Toast.LENGTH_LONG).show();
-                            //JSONObject jsonObject = new JSONObject(response)
-                            json = new JSONObject(response);
+                            //Toast.makeText(GamersOnline.this, "Made IT", Toast.LENGTH_LONG).show();
+                            JSONObject json = new JSONObject(response);
                             System.out.println(json.toString(4));
-                            successLogin = true;
+                            Boolean hasError = json.getBoolean("error");
+                            String message = json.getString("message");
+                            if(!hasError){
+                                if(message.matches("Logged In.")) {
+                                    login(json.getString("User"), json.getString("Name"),
+                                            json.getString("Image") ,json.getString("Currency"));
+                                }
+                                else{
+                                    Toast.makeText(GamersOnline.this, message, Toast.LENGTH_LONG).show();
+                                }
+                                json = null;
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -54,6 +75,8 @@ public class GamersOnline extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        System.out.println("\n\t---\tError response.\n");
+                        System.out.println("\n\t---\t" + error.getMessage() + "\n");
                     }
                 }) {
             @Override
@@ -61,30 +84,37 @@ public class GamersOnline extends AppCompatActivity {
                 EditText emailIn = (EditText)findViewById(R.id.email);
                 TextView passIn = (TextView)findViewById(R.id.pass);
                 Map<String, String> params = new HashMap<>();
-                System.out.println("\n\tUser emaail: " + emailIn.getText().toString());
+                System.out.println("\tUser emaail: " + emailIn.getText().toString());
                 System.out.println("\t User pass: " + passIn.getText().toString());
-                params.put("username", emailIn.getText().toString());
-                params.put("password", passIn.getText().toString());
+                params.put("Email", emailIn.getText().toString());
+                params.put("Password", passIn.getText().toString());
                 return params;
             }
         };
 
         RequestQueue requestQueue = Volley.newRequestQueue(GamersOnline.this);
         requestQueue.add(stringRequest);
+        buttonPressed = false;
     }
 
+    //Function tied to the sign in button
     public void tryLogin(View view) {
         getStuff();
-        System.out.println("\tCan login: " + successLogin);
-        if(successLogin){
-            Intent in = new Intent(this, ProfileView.class);
-            try{
-                in.putExtra("profileName", json.getString("user_id"));
-                startActivity(in);
-            }
-            catch (JSONException e){
-                e.printStackTrace();
-            }
-        }
+    }
+
+    public void login(String userID,String userName,String imageInText,String userCurrency){
+        if(buttonPressed) return;
+        buttonPressed = true;
+        Intent in = new Intent(this, ProfileView.class);
+        byte[] decode = Base64.decode(imageInText, Base64.DEFAULT);
+        Bitmap imageBitmap = BitmapFactory.decodeByteArray(decode, 0, decode.length);
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.PNG, 50, bs);
+        in.putExtra("playerId", userID)
+                .putExtra("profileName", userName)
+                .putExtra("playerMoney", userCurrency)
+                .putExtra("profileImage", bs.toByteArray());
+
+        startActivity(in);
     }
 }
--- GamersOnlineApp/app/src/main/java/com/example/jjnjs/gamersonline/ProfileView.java
+++ GamersOnlineApp/app/src/main/java/com/example/jjnjs/gamersonline/ProfileView.java
@@ -2,10 +2,14 @@ package com.example.jjnjs.gamersonline;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -24,23 +28,85 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProfileView extends AppCompatActivity{
+    JSONObject profileJSON;
+
+    private String profileName = "";
+    private String profileId = "";
+    private String profileImageRaw = "";
+    private String profileCurrency = "";
+
+    private TextView userNameTextView;
+    private TextView userCurrencyTextView;
+    private ImageView userProfileImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        userNameTextView = findViewById(R.id.userName);
+        userNameTextView.setText(getIntent().getStringExtra("profileName"));
+        userNameTextView = null;
+        if(getIntent().hasExtra("profileImage")){
+            userProfileImageView = findViewById(R.id.profileImage);
+            Bitmap b = BitmapFactory.decodeByteArray(
+                    getIntent().getByteArrayExtra("profileImage"),0,
+                    getIntent().getByteArrayExtra("profileImage").length
+            );
+            userProfileImageView.setImageBitmap(b);
+            userProfileImageView = null;
+        }
+        if(getIntent().hasExtra("playerMoney")){
+            userCurrencyTextView = findViewById(R.id.currencyTextView);
+            userCurrencyTextView.setText(
+                    "Currency: " + getIntent().getStringExtra("playerMoney") + " shekel");
+        }
+        fetchServerImage();
     }
 
-    public void getImage(){
-        Bitmap imageBitmap;
-        /*
-        byte[] decode = Base64.decode(jsonObject.getString("Image String Name"),0);
-        imagebitmap = BitmapFactory.decodeByteArray(decode, 0, decode.length);
-        ImageName.setImageDrawable(imagebitmap);
-        */
+    public void fetchServerImage(){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://18.219.55.178/GamersOnline/Methods/server.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //Toast.makeText(GamersOnline.this, "Made IT", Toast.LENGTH_LONG).show();
+                            JSONObject json = new JSONObject(response);
+                            Boolean hasError = json.getBoolean("error");
+                            String message = json.getString("message");
+                            if(hasError){
+                                Toast.makeText(ProfileView.this, message, Toast.LENGTH_LONG).show();
+                            } else {
+                                if(json.has("Image")){
+                                    byte[] decode = Base64.decode(json.getString("Image"), Base64.DEFAULT);
+                                    Bitmap imageBitmap = BitmapFactory.decodeByteArray(decode, 0, decode.length);
+                                    BitmapDrawable bd = new BitmapDrawable(imageBitmap);
+                                    findViewById(R.id.MainLayout).setBackgroundDrawable(bd);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("\n\t---\tError response.\n");
+                        System.out.println("\n\t---\t" + error.getMessage() + "\n");
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("User", getIntent().getStringExtra("playerId"));
+                return params;
+            }
+        };
 
-        //return imageBitmap;
+        RequestQueue requestQueue = Volley.newRequestQueue(ProfileView.this);
+        requestQueue.add(stringRequest);
     }
 
     public void showStats(View view) {
+
     }
 }
